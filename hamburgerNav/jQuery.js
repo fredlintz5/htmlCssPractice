94e18f3b9c3f7eb0d966d07bf619aa6088b02ea1
Fred Lintz <fredlintz@Freds-MacBook-Pro.local>
2017-07-31 11:00:06 -0600

    removed Bootstrap, fixed css for hidden navNarrow div

--- hamburgerNav/jQuery.js
+++ hamburgerNav/jQuery.js
@@ -1,13 +1,14 @@
 $(document).ready(function() { 
-
 	
-
+	//when hamburger clicked, toggle display to visible
+	//and then hidden when clicked again
 	$('#showNarrow').click(function() {
 		$('#navNarrow').toggle();
 
 	});
 	
-
+	//if window goes above media query breakpoint,
+	//return navNarrow display to hidden
 	$(window).resize(function() {
 
 		if ($(window).width() >= 576) {
@@ -15,21 +16,9 @@ $(document).ready(function() {
 		}
 
 	});
-
-
 	
-
-
-		
-
-
-		
-	
-
-
 
 });
-
 
 
 
